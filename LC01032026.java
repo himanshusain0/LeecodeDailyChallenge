ed44f3727a2f7b16e719c5bae431e40ecc65c583
himanshusain0 <ht0257445@gmail.com>
2026-03-01 10:13:08 +0530

    l

---
+++ LC01032026.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minPartitions(String n) {
+        char ans = '0';
+        for(int i =0;i<n.length();i++){
+            if(n.charAt(i)>ans){
+                ans = n.charAt(i);
+            }
+        }
+        return ans-'0';
+    }
+}
